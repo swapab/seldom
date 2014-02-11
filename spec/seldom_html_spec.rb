11d514e624ff3aa54bdd3e6b1a328080245cac27
Swapnil Abnave <swapnil.abnave@kiprosh.com>
2014-02-11 12:46:25 +0530

    Better indentation

--- spec/seldom_html_spec.rb
+++ spec/seldom_html_spec.rb
@@ -1,33 +1,33 @@
 require 'spec_helper'
 
 describe Seldom::Html do
-	before do
-		@seldom_h = described_class.new
-	end
+  before do
+    @seldom_h = described_class.new
+  end
 
-	describe '#initialize' do
-		it 'should initialize with default options' do
-			@options = @seldom_h.options
-			expect(@options).not_to be_empty
-			@options.should be_a(Hash)
-			@options.fetch(:content_selector).should eq '#right-content'
-			@options.fetch(:nav_selector).should eq '#nav'
-		end
-	end
+  describe '#initialize' do
+    it 'should initialize with default options' do
+      @options = @seldom_h.options
+      expect(@options).not_to be_empty
+      @options.should be_a(Hash)
+      @options.fetch(:content_selector).should eq '#right-content'
+      @options.fetch(:nav_selector).should eq '#nav'
+    end
+  end
 
-	describe '#selector_to_html' do
-		it 'form a default div tag' do
-			@seldom_h.selector_to_html('#the-container').should include "<div id=the-container>"
-		end
+  describe '#selector_to_html' do
+    it 'form a default div tag' do
+      @seldom_h.selector_to_html('#the-container').should include "<div id=the-container>"
+    end
 
-		it 'form a span tag' do
-			@seldom_h.selector_to_html('#span-el', 'span').should include "<span id=span-el>"
-		end
-	end
+    it 'form a span tag' do
+      @seldom_h.selector_to_html('#span-el', 'span').should include "<span id=span-el>"
+    end
+  end
 
-	describe '#body' do
-		it 'should generate an html tag with sample content' do
-			@seldom_h.body.should include "<html><body><h1>Hello, World!</h1>"
-		end
-	end
+  describe '#body' do
+    it 'should generate an html tag with sample content' do
+      @seldom_h.body.should include "<html><body><h1>Hello, World!</h1>"
+    end
+  end
 end
